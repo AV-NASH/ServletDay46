87beadc24661d139ab78c3ae7c5e2016793fb392
AV-NASH <avinash21213434@gmail.com>
2020-11-30 00:30:52 +0530

    Added servlet program demonstrating hello world and simple login page

---
+++ src/main/java/com/cg/firstservlet/FirstServlet.java
@@ -0,0 +1,20 @@
+package com.cg.firstservlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/First")
+public class FirstServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("text/html");
+        PrintWriter out=response.getWriter();
+        out.println("<h3>Heelo world<h3>");
+        out.close();
+
+    }
+}
